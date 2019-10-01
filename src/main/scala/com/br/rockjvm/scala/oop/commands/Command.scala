7352bd5217f1838e21ac4c1b4e4df6d75d0701c9
Amom Mendes Luiz <p000amluiz@prservicos.com.br>
2019-09-30 22:03:40 -0300

    Starting pwd

--- src/main/scala/com/br/rockjvm/scala/oop/commands/Command.scala
+++ src/main/scala/com/br/rockjvm/scala/oop/commands/Command.scala
@@ -10,7 +10,39 @@ trait Command {
 
 }
 
+<<<<<<< Updated upstream
 object Command{
   def from (input: String):Command =
     new UnknownCommand
+=======
+object  Command {
+  val MKDIR = "mkdir"
+  val LS = "ls"
+  val PWD = "pwd"
+  def emptyCommand:Command = (state: State) => state
+
+  def incompleteCommand(name:String) : Command = new Command {
+    override def apply(state:State): State =
+      state.setMessage("The command "+ name + " is incomplete dud!")
+
+  }
+
+  def from(input: String): Command = {
+    val tokens: Array[String] = input.split("\\s");
+    if (input.isEmpty || tokens.isEmpty) {
+      emptyCommand
+    } else if (MKDIR.equals(tokens(0))) {
+        if (tokens.length < 2) incompleteCommand(MKDIR)
+        else new MkDir (tokens(1))
+
+    } else if(LS.equals(tokens(0))) {
+        new Ls
+
+    } else if(PWD.equals(tokens(0))) {
+      new Pwd
+    }else {
+        new UnknownCommand
+    }
+  }
+>>>>>>> Stashed changes
 }
